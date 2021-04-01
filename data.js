57050833cd334c3cab99a6b79117e517f8b4954a
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-04-02 02:23:23 +0530

    Converted Small Repeating Functionalities to Functions

--- data.js
+++ data.js
@@ -19,7 +19,7 @@ btn2.addEventListener("click",()=>{
     5. A function for Creating A span Tag with certain attributes.
     6. A function for Creating A anchor Tag with certain attributes.
 */
-const brTag=document.createElement('br');
+const brTag= ()=>{return document.createElement('br')};
 //* A function to provide some sleep time in order to provide some time for second chart to appear
 const sleep = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -29,7 +29,7 @@ const sleep = (ms) => {
 async function createSpan()
 {
     var span=document.createElement('span');
-    span.classList.add('white');
+    span.style.display.fontsize="40px";
     return span;
 }
 
@@ -47,11 +47,10 @@ async function getFollowers(element,url)
         //* Bcoz we are only interested in finding number of followers and not their additional thing
         res=res.length
         let span=await createSpan()
-        element.appendChild(brTag);
         span.appendChild(document.createTextNode(`Total Number of followers=${res}`));
-        element.appendChild(brTag);
+        element.appendChild(brTag());
         element.appendChild(span);
-        element.appendChild(brTag);
+        element.appendChild(brTag());
         
     }
     xhr.send()   
@@ -65,24 +64,37 @@ async function getFollowing(element,url)
     var res=""
     xhr.onload=async function()
     {
-        console.log('Res is ',element);
         res=await xhr.response;
         res=JSON.parse(res)
         //* Bcoz we are only interested in finding number of followers and not their additional thing
-        res=res.length
-        element.appendChild(document.createTextNode(`Total Number of following=${res+10}`));
+        res=res.length;
+        element.appendChild(brTag());
+        element.appendChild(document.createTextNode(`Total Number of following=${res}`));
     }
     xhr.send()
 }
 
 
+const getImgTag = (index, url) =>{
+    const img = document.createElement('img');
+    img.src=url;
+    img.classList.add('circularimage');
+}
+
+const getAnchorTag = (url) =>{
+    const aTag= document.createElement('a');
+    aTag.setAttribute('href',url);
+    aTag.appendChild(document.createTextNode('Link to the Profile'))
+    aTag.appendChild(brTag());
+    aTag.style.color="black";
+    return aTag;
+}
 //Function for Drawing Chart
 
 async function draw1(mymap,index)
 {
-
+    //Sleep is used in order to give it some time after one element
     await sleep(1000);
-    console.log("Mymap in draw",mymap,index);
     google.charts.load('current', {'packages':['corechart']});
     await google.charts.setOnLoadCallback(drawChart);
     
@@ -99,8 +111,7 @@ async function draw1(mymap,index)
    
     var para=await t2darr.map((ele)=>{
         return ele
-    })
-    console.log(para,t2darr);
+    });
     para.unshift(['Task',' Hours Per Day'])
     var data = google.visualization.arrayToDataTable(para);
     var options = {
@@ -118,6 +129,8 @@ async function draw1(mymap,index)
         chart.draw(data, options);
     }   
 }
+
+
 
 
 
@@ -149,28 +162,18 @@ async function afterload(name,value,url,res,index)
             xhr.send()
         }
     });
-    console.log(mymap);
-    await draw1(mymap,index)
     const parent=name.parentElement;
-    console.log("Parent is",parent);
-    const followers=await getFollowers(parent,res[0].owner.followers_url)
-    const imgTag=document.getElementById(`img${index}`)
-    imgTag.src=res[0].owner.avatar_url
-    imgTag.classList.add('circularimage')
-    const atag=document.createElement("A")
-    atag.setAttribute('href',res[0].owner.html_url)
-    atag.appendChild(document.createTextNode('Link to the Profile'))
-    atag.appendChild(document.createElement('br'));
-    atag.style.color="black";
-    parent.appendChild(atag)
-    const brtag=document.createElement('br')
-    var span=document.createElement('span')
-    span.classList.add('white')
+    await draw1(mymap,index);
+    await getFollowers(parent,res[0].owner.followers_url)
+    getImgTag(index, res[0].owner.avatar_url);
+    parent.appendChild(await getAnchorTag(res[0].owner.html_url));
+    const span=await createSpan();
     span.appendChild(document.createTextNode(`Total number of repositories ${res.length}`))
     parent.appendChild(span)
     getFollowing(parent,url);
 }
-   
+
+
 function getRepo(name,index)
 {
     var value=name.value;
@@ -182,6 +185,7 @@ function getRepo(name,index)
     });
     xhr.send();
 }  
+
 
 window.onload = async ()=>{
     document.getElementById('reposbtn1').click();
